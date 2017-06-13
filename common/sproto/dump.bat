a76bef80dba1c0a3e08ecc5ece5c5869dbe2e028
ivanz <losetear@gmail.com>
2017-06-13 15:41:56 +0800

    删除打包

--- common/sproto/dump.bat
+++
@@ -1,2 +0,0 @@
-lua sprotodump.lua -cs game_proto.sproto game_protoc2s.sproto game_protos2c.sproto login_protoc2s.sproto -d SprotoCS\
-xcopy SprotoCS\*.* ..\..\client\GameTemplateClient\Assets\Scripts\SprotoCS\ /Y
