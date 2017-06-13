413b290bd02616483de418817b196b37d22a44ca
ivanz <losetear@gmail.com>
2017-06-13 14:30:21 +0800

    支持导出后拷贝

--- common/sproto/dump.bat
+++ common/sproto/dump.bat
@@ -1,1 +1,2 @@
 lua sprotodump.lua -cs game_proto.sproto game_protoc2s.sproto game_protos2c.sproto login_protoc2s.sproto -d SprotoCS\
+xcopy SprotoCS\*.* ..\..\client\GameTemplateClient\Assets\Scripts\SprotoCS\ /Y
